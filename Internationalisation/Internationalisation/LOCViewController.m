b357bf68e38d171df5aa48938a98b446683860db
Sunith Murali <sunith007@outlook.com>
2014-04-05 11:57:45 +0530

    Initail commit
    commit

---
+++ Internationalisation/Internationalisation/LOCViewController.m
@@ -0,0 +1,36 @@
+//
+//  LOCViewController.m
+//  Internationalisation
+//
+//  Created by mac on 4/3/14.
+//  Copyright (c) 2014 Sunith Muralidharan Pillai. All rights reserved.
+//
+
+#import "LOCViewController.h"
+
+@interface LOCViewController ()
+{
+    IBOutlet UILabel *lbl1;
+    
+    IBOutlet UILabel *lbl2;
+}
+@end
+
+@implementation LOCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    lbl1.text = [NSString stringWithFormat:NSLocalizedString(@"hi",nil)];
+    
+    lbl2.text = [NSString stringWithFormat:NSLocalizedString(@"hai",nil)];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Internationalisation/InternationalisationTests/InternationalisationTests.m
@@ -0,0 +1,34 @@
+//
+//  InternationalisationTests.m
+//  InternationalisationTests
+//
+//  Created by mac on 4/3/14.
+//  Copyright (c) 2014 Sunith Muralidharan Pillai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InternationalisationTests : XCTestCase
+
+@end
+
+@implementation InternationalisationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
